81f3608348b92f502ecb31602b29a417935c0a39
Nicholas Maloof <nmaloof925@gmail.com>
2023-11-24 16:24:52 -0500

    Render matchviews upon searching for a players name

--- modules/frontend/src/main/scala/gametracker/SearchBox.scala
+++ modules/frontend/src/main/scala/gametracker/SearchBox.scala
@@ -1,4 +1,4 @@
-package gametracker
+package gametracker.frontend
 
 import com.raquo.laminar.api.L.{*, given}
 import io.laminext.fetch.{Fetch, FetchResponse}
@@ -9,35 +9,37 @@ import io.circe.generic.semiauto.*
 import io.circe.{Decoder, Encoder}
 
 import concurrent.ExecutionContext.Implicits.global
-
+import gametracker.shared.domain.*
 
 object SearchBox {
 
-   val searchStringVar = Var("")
-   
+  given playerDecoder: Decoder[Player]           = deriveDecoder[Player]
+  given gameDecoder: Decoder[Game]           = deriveDecoder[Game]
+  given matchViewDecoder: Decoder[MatchView] = deriveDecoder[MatchView]
+  
+  val searchStringVar = Var("")
 
-   case class Game(id: Int, name: String)
-   given gameDecoder: Decoder[Game] = deriveDecoder[Game]
-   
-   def render(formSubmitted: Observer[Boolean], responseReceived: FetchResponse[List[Game]] => Unit): Element = {
+   def render(formSubmitted: Observer[Boolean], responseReceived: FetchResponse[List[MatchView]] => Unit): Element = {
       div(
-         cls := "flex justify-center",
-         input(
-            placeholder := "Player Name",
-            controlled(
-               value <-- searchStringVar.signal,
-               onInput.mapToValue --> searchStringVar.writer
-            )
-         ),
-         button(
-            "Search",
-            onClick.mapTo(searchStringVar.now()).flatMap { case (searchString) =>
-               Fetch.get("http://localhost:8080/game/all").decode[List[Game]].map {r =>
-                  formSubmitted.onNext(true)
-                  r
-               }
-            } --> responseReceived
-         )
+        cls := "flex justify-center",
+        input(
+          placeholder := "Player Name",
+          controlled(
+            value <-- searchStringVar.signal,
+            onInput.mapToValue --> searchStringVar.writer
+          )
+        ),
+        button(
+          "Search",
+          onClick.mapTo(searchStringVar.now()).flatMap { case (searchString) =>
+            Fetch.get(s"http://localhost:8080/player/name/$searchString").decode[Player].flatMap { player => 
+              formSubmitted.onNext(true)
+              Fetch.get(s"http://localhost:8080/match/search?playerId=${player.data.id}").decode[List[MatchView]].map { mv =>
+                mv
+              }
+            }
+          } --> responseReceived
+        )
       )
    }
 }
